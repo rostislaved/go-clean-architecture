363eba517ba632295e899a0a5a366be496e57361
Rostislav <rostislaved@gmail.com>
2024-10-28 01:55:05 +0200

    dev

--- internal/app/domain/book/book.go
+++ internal/app/domain/book/book.go
@@ -3,9 +3,9 @@ package book
 import "time"
 
 type Book struct {
-	ID            int64     `db:"id"`
-	Name          string    `db:"name"   validate:"required"`
-	Author        string    `db:"author" validate:"required"`
-	Date          time.Time `db:"date"   validate:"required,datetime"`
-	NumberOfPages int       `db:"author" validate:"required"`
+	ID            int64
+	Name          string
+	Author        string
+	Date          time.Time
+	NumberOfPages int
 }
